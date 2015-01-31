b030ff0da6e7c39ec0bcf413673f7c40c5c675c1
Andrew Cousins <acsky713@gmail.com>
2015-01-30 16:26:22 -0800

    The snake will now stop when it hits a wall or itself and display an error message.  It also can eat food to grow longer and new food will spawn when the old food is eaten.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ use2D = true;
 
 // -- GRID --
 
+//The grid holds the positions of things in the world
 var squareSize = 10;
 //the size of each square
 var gridSize = 60;
@@ -19,25 +20,27 @@ function clearGrid() {
 clearGrid();
 //call it first initially
 
-//draws all the objects in the grid
-function drawGrid() {
-	
-	//Clear the Grid
-	clearGrid();
-	//put things into the grid
-	putSnakeInGrid();
-	putFoodInGrid();
-	//iterate through the whole grid and draw things
-	for (var x = 0; x < grid.length; x++) {
-		for (var y = 0; y < grid.length; y++) {
-			if (grid[x][y] == 'food') {//if its food draw food
-				drawFood(x * squareSize, y * squareSize);
-			} else if (grid[x][y] == 'snake') {//if its snake draw snake
-				drawSnake(x * squareSize, y * squareSize);
-			}
-		}
-	}
-}
+//Draw grid is no longer used
+
+// //draws all the objects in the grid
+// function drawGrid() {
+//
+// //Clear the Grid
+// clearGrid();
+// //put things into the grid
+// putSnakeInGrid();
+// putFoodInGrid();
+// //iterate through the whole grid and draw things
+// for (var x = 0; x < grid.length; x++) {
+// for (var y = 0; y < grid.length; y++) {
+// if (grid[x][y] == 'food') {//if its food draw food
+// drawFood(x * squareSize, y * squareSize);
+// } else if (grid[x][y] == 'snake') {//if its snake draw snake
+// drawSnake(x * squareSize, y * squareSize);
+// }
+// }
+// }
+// }
 
 // -- SNAKE --
 
@@ -46,7 +49,7 @@ var snakeSize = 10;
 var snakeDir = 'r';
 //the direction the snake is moving in.
 //the snake is an array of objects with an x and y value.
-var snake = new Array(10);
+var snake = new Array(snakeSize);
 function newSnake() {
 	var snakeX = snakeSize, //starting location for the snake's head
 	    snakeY = 0;
@@ -54,18 +57,19 @@ function newSnake() {
 		snake[i] = {//adds each object with an x and y value
 			x : snakeX - i,
 			y : snakeY,
-			snakeSprite: drawSnake(snakeX - i, snakeY)
+			snakeSprite : drawSnake(snakeX - i, snakeY)
 		};
+		//puts the snake in the grid
+		grid[snakeX - i][snakeY] = 'snake';
 	}
 
 }
 
-
 //draws one snake block in the world
 //Param: x,y coordinates of grid, offset is delt with here
 function drawSnake(x, y) {
-	x = x*squareSize;
-	y = y*squareSize;
+	x = x * squareSize;
+	y = y * squareSize;
 	var snakeSprite = new Sprite();
 	snakeSprite.height = squareSize;
 	snakeSprite.width = squareSize;
@@ -80,25 +84,50 @@ function drawSnake(x, y) {
 function moveSnake() {
 	//only need to move the square at the back to the front
 	//moves the last square in the array to the front of the array.
-	snake.unshift(snake.pop());
+	var temp = snake.pop();
+	grid[temp.x][temp.y] = '';
+	snake.unshift(temp);
 	//snake[0] needs to be the current head
 	//snake[1] is the current head
 	snake[0].x = snake[1].x;
 	snake[0].y = snake[1].y;
-	if(snakeDir == 'r'){
+	if (snakeDir == 'r') {
 		snake[0].x++;
-	}
-	else if(snakeDir == 'l'){
+	} else if (snakeDir == 'l') {
 		snake[0].x--;
-	}
-	else if(snakeDir == 'u'){
+	} else if (snakeDir == 'u') {
 		snake[0].y--;
-	}
-	else if(snakeDir == 'd'){
+	} else if (snakeDir == 'd') {
 		snake[0].y++;
 	}
+	checkCollision(snake[0].x, snake[0].y);
+	grid[snake[0].x][snake[0].y] = 'snake';
 	snake[0].snakeSprite.x = snake[0].x * squareSize;
 	snake[0].snakeSprite.y = snake[0].y * squareSize;
+}
+
+//checks if the snake is hitting the wall, itself, or eating food.
+function checkCollision(x, y) {
+	if (x < 0 || y < 0 || x >= gridSize || y >= gridSize) {
+		alert("GAME OVER");
+	} else {
+		var check = grid[x][y];
+		if (check == 'snake') {
+			alert("GAME OVER");
+		} else if (check == 'food') {
+			//make a new snake segment
+			//we'll put it on the end and make its locations the current ones,
+			//since it will be changed the next snake update
+			snake.push({
+				x : x,
+				y : y,
+				snakeSprite : drawSnake(x, y)
+			});
+			//then we delete the food and make new food
+			world.removeChild(food.pop().foodSprite);
+			newFood();
+		}
+	}
 }
 
 // -- FOOD --
@@ -114,7 +143,7 @@ function newFood() {
 	food.push({
 		x : randX,
 		y : randY,
-		foodSprite: drawFood(randX, randY)
+		foodSprite : drawFood(randX, randY)
 	});
 	grid[randX][randY] = 'food';
 	//tells the grid where the food is
@@ -123,8 +152,8 @@ function newFood() {
 //draws the food in the world
 //param: x,y coordinates of the grid, offset is taken care of.
 function drawFood(x, y) {
-	x = x*squareSize;
-	y = y*squareSize;
+	x = x * squareSize;
+	y = y * squareSize;
 	var foodSprite = new Sprite();
 	foodSprite.height = squareSize;
 	foodSprite.width = squareSize;
@@ -139,29 +168,29 @@ function drawFood(x, y) {
 
 //snake will be controlled with the WASD keys
 //Left
-gInput.addBool(65, "left");//a
+gInput.addBool(65, "left");
+//a
 // //Right
-gInput.addBool(68, "right");//d
+gInput.addBool(68, "right");
+//d
 // //Down
-gInput.addBool(83, "down");//s
+gInput.addBool(83, "down");
+//s
 // //Up
-gInput.addBool(87, "up");//w
+gInput.addBool(87, "up");
+//w
 
-function checkDir(){
-	if(gInput.right && snakeDir != 'l'){
+function checkDir() {
+	if (gInput.right && snakeDir != 'l') {
 		snakeDir = 'r';
-	}
-	else if(gInput.left && snakeDir != 'r'){
+	} else if (gInput.left && snakeDir != 'r') {
 		snakeDir = 'l';
-	}
-	else if(gInput.down && snakeDir != 'u'){
+	} else if (gInput.down && snakeDir != 'u') {
 		snakeDir = 'd';
-	}
-	else if(gInput.up && snakeDir != 'd'){
+	} else if (gInput.up && snakeDir != 'd') {
 		snakeDir = 'u';
 	}
 }
-
 
 // -- Start the Game --
 newSnake();
